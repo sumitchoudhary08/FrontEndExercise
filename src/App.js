a40d4c5ecc1c4d75096e7f6cb87ebb21ce764f37
SumitChoudhary <sumitbca93@gmail.com>
2019-05-16 16:48:54 +0530

    Use cookies and local storage

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import {Dropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
 import Products from './components/Products/Products';
 import ProductData from './assets/files/ProductData.json';
+import Cookies from 'js-cookie';
 
 class App extends PureComponent{
 
   state={
     //products: ProductData.products,
     dropdownOpen: false,
-    dropDownValue: "INR",
-    conversionValue: 1
+    //dropDownValue: localStorage.getItem("ddlValue") === null ? "INR" : localStorage.getItem("ddlValue"),
+    //conversionValue: localStorage.getItem("ddlValue") === null || localStorage.getItem("ddlValue") === "INR" ? 1 : localStorage.getItem("cValue")
+    dropDownValue: document.cookie.indexOf('ddlValue') ===  -1 ? "INR" : Cookies.get('ddlValue'),
+    conversionValue: document.cookie.indexOf('ddlValue') === -1 || Cookies.get('ddlValue') === "INR" ? 1 : Cookies.get("cValue")
   }
 
   toggleHandler = () => {
@@ -23,11 +26,18 @@ class App extends PureComponent{
     var currentSelection = event.target.textContent;
     var oldSelection = this.state.dropDownValue
     //console.log(ProductData);
+    //localStorage.setItem("ddlValue", currentSelection);
+    Cookies.set("ddlValue", currentSelection);
 
     if(currentSelection !== oldSelection){
       fetch('https://api.exchangeratesapi.io/latest?base=INR')
       .then(response => response.json())
-      .then(data => this.setState({dropDownValue : currentSelection, conversionValue: (oldSelection === "INR" ? data.rates.USD : 1)}));
+      .then(data => {
+        //localStorage.setItem('cValue', (oldSelection === "INR" ? data.rates.USD : 1));
+        Cookies.set('cValue', (oldSelection === "INR" ? data.rates.USD : 1));
+        this.setState({dropDownValue : currentSelection, conversionValue: (oldSelection === "INR" ? data.rates.USD : 1)})
+      })
+      .catch(error => console.log(error))
     }
   }
 
